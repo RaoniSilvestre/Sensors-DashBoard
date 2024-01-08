f08f90c92f955378354328e3de1be321d6446eff
Raoni Silva <raoni.silvestre@hotmail.com>
2024-01-08 17:08:19 -0300

    Refactor OLEDHandler.cpp for improved readability and formatting

--- IoT/src/OLEDHandler.cpp
+++ IoT/src/OLEDHandler.cpp
@@ -2,7 +2,8 @@
 
 Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &WIRE);
 
-void setupOLED(){
+void setupOLED()
+{
   Serial.println("OLED FeatherWing test");
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
@@ -23,18 +24,19 @@ void setupOLED(){
   // text display tests
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0,0);
+  display.setCursor(0, 0);
   display.print("Connecting to SSID\n'adafruit':");
   display.print("connected!");
   display.println("IP: 192.168.0.1");
   display.println("Sending val #0");
-  display.setCursor(0,0);
+  display.setCursor(0, 0);
   display.display(); // actually display all of the above
 }
 
-void updateOLED(){
+void updateOLED()
+{
   display.clearDisplay();
-  display.setCursor(0,0);
+  display.setCursor(0, 0);
   display.setTextSize(1);
   display.print("Temperature: ");
   display.print(getTemperature());
@@ -43,6 +45,6 @@ void updateOLED(){
   display.print(getHumidity());
   display.println("%");
   display.print("Moisture: ");
-  display.print(getMoisture());
+  display.println(getMoisture());
   display.display();
 }
